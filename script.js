641b8cb673a2120c0e44f209380399450099dddd
Sergeu <sergeu.cher96@gmail.com>
2024-07-08 12:52:33 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleClick() {
+    window.location.href = 'https://t.me/gamee?start=ref_798214032';
+}
+
+    // Здесь можно добавить любой другой функционал, например, переход по ссылке
+    // window.location.href = 'https://example.com';
